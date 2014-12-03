a41f341e166fb609725c798bdee7c47ddd345745
ChuBaka13321 <BrianChu13321@gmail.com>
2014-12-02 17:20:28 -0800

    Add postgres compatibility to .travis.yml

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,12 +3,10 @@
 #All of our factory declarations get wrapped in a FactoryGirl.define block
 FactoryGirl.define do
 
-  #we define a factory name, and then we specify the class
   factory :a_user, class: User do
 
-    #these setup the attributes, each declaration is a method call
-    email "x@y.com"
-    password "gemini"
+    email ""
+    password ""
 
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,25 @@
 require "rails_helper"
 
+## note that due to the way we included this in our gemfile
+#  namely 'require: false', means that we must explicitly include
+#  it here
+require 'shoulda/matchers'
 
 describe User do
 
     it "has an email" do
-      expect(user.email).to_not be_nil  
+      user = FactoryGirl.create(:a_user)
+      expect(user.email).to_not be_nil
     end
 
     it "has a password" do
+      user = FactoryGirl.create(:a_user)
       expect(user.password).to_not be_nil
     end
 
+    ## our new tests that examine our User model to determine if 
+    #  a validation exists requiring the presence of the attribute
+    it { should validate_presence_of(:email) }
+
+    it { should validate_presence_of(:password) }
 end
